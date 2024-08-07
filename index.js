4fe374deb554a51d2aa958fe11db80f421006f14
Gabriel Kung <69708951+ieatyoursushi@users.noreply.github.com>
2024-08-07 23:34:57 +0000

    latest

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const fs = require('fs');
 //read line module
 const readline = require('readline');
-const Database = require("@replit/database")
+const Database = require("@replit/database");
 //createServer(p1, p2) is a mutator method that returns an instance of the Server class which takes 2 paremeters 
 const server = http.createServer((request, response) => {
     // Enable CORS for all routes
@@ -22,7 +22,6 @@ const server = http.createServer((request, response) => {
                 });
                 request.on('end', () => {
                     const user = JSON.parse(userData);
-                    console.log(user);
                     //store data in file with the purpose of making history 
                     fs.appendFile('history.txt', user.name + '\n' + user.message + '\n' + user.postId + '\n' + user.postDate + '\n\n', (err) => {
                        if(err) throw err;
@@ -90,6 +89,58 @@ const server = http.createServer((request, response) => {
                     comments.push(userInstance);
                 }
                 response.write(JSON.stringify(comments));
+                //console.count(`sent to ${request.headers.referer}`);
+                response.end();
+            })
+            
+        } else if (request.url === "/api/history/last") {
+            class Comment {
+                constructor(uName, uMessage, uId, uDate) {
+                    this.name = uName;
+                    this.message = uMessage;
+                    this.Id = uId;
+                    this.date = uDate;
+                }
+                static printToFile(array) {
+                    fs.appendFile('lines.json',"\n"+  JSON.stringify(array, null, 2), (err) => {
+                        if(err) throw err;
+                    })
+                }
+            }
+            let comments = [];
+            fs.readFile('history.txt', 'utf8', (err, data) => {
+                if(err) throw err;
+                //where I get the data, u(n) = 5n, m(n) = 5n + 1, I(n) = 4n + 2
+                const lines = data.split('\n');
+                const lastLine = lines.length-2;
+                let usernames = [];
+                let messages = [];
+                let postIds = [];
+                let dates = [];
+                
+                for(let i = 0; i < lastLine;i++) {
+                    if(i%5 == 0) {
+                        //console.log("user (" + g + "): " + lines[i]);
+                        usernames.push(lines[i]);
+                    } else if(i%5 == 1) {
+                        //console.log("message (" + g + "): " + lines[i]);
+                        messages.push(lines[i]);
+                    } else if (i%5 == 2) {
+                        postIds.push(lines[i]);
+                    } else if (i%5 == 3) {
+                        dates.push(lines[i]);
+                    }
+                }
+                //object array that will be sent to requester
+                for(let i = 0; i < usernames.length; i++) {
+                    let userInstance = new Comment(usernames[i], messages[i], postIds[i], dates[i]);
+                    comments.push(userInstance);
+                }
+                if(comments.length >= 1) {
+                    response.write(JSON.stringify([comments[comments.length-2], comments[comments.length-1]]));
+                } else {
+                    response.write(JSON.stringify([comments[comments.length-1]]));
+                }
                 //console.count(`sent to ${request.headers.referer}`);
                 response.end();
             })
